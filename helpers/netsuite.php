de654dcfbd4278a45119d5fc32b35176541c9d0c
Michael Bianco <mbianco@ascensionpress.com>
2010-08-03 10:58:18 -0400

    Adding initial helper

---
+++ helpers/netsuite.php
@@ -0,0 +1,81 @@
+<?
+class netsuite {
+	public static $netsuiteConnection;
+	
+	public static function getNetsuiteConnection() {
+		if(empty(netsuite::$netsuiteConnection)) {
+			// setup the netsuite connection
+			ini_set('include_path', ini_get('include_path').PATH_SEPARATOR.SYSPATH.'vendor/netsuite');
+
+			require SYSPATH."vendor/netsuite/PHPtoolkit.php";
+			require SYSPATH."vendor/netsuite/directory_v2010.1.php";
+
+			netsuite::$netsuiteConnection = new nsClient(nsHost::live);
+
+			// set request level credentials. (email, password, account#, internal id of role)
+			netsuite::$netsuiteConnection->setPassport(
+				Kohana::config('netsuite.email'),
+				Kohana::config('netsuite.password'),
+				Kohana::config('netsuite.account'),
+				Kohana::config('netsuite.role')
+			);
+		}
+		
+		return netsuite::$netsuiteConnection;		
+	}
+	
+	public static function bestMatchContact($data) {
+		// this is the tricky function: contacts can be stored in many different ways
+		// Here are a couple different cases:
+		//		First & last name match but email doesn't (b/c the user is using a different email)
+		//		First & last name match but zip doesn't (but the state does... maybe the user moved)
+		//		We have the date modified and it was a year ago, the state, first, and last matches but the zip / email are different
+		
+	}
+	
+	public static function findContact($data) {
+		$netsuiteClientConnection = netsuite::getNetsuiteConnection();
+		
+		$searchFields = array();
+		
+		// data should be an array of searchKey => searchValue i.e. array('email' => 'hello@gmail.com', 'firstName' => 'hello')
+
+		foreach($data as $searchKey => $searchValue) {
+			$searchFields[$searchKey] = array(
+				'operator' => 'is',
+				'searchValue' => $searchValue
+			);
+		}
+		
+		// find record
+		
+		$contactRecordSearch = new nsComplexObject("ContactSearchBasic");
+		$contactRecordSearch->setFields(array_merge($setFields, array(
+			// 'recType' => 'contact',
+		)));
+		
+		// FALSE sets bodyFieldsOnly
+		$netsuiteClientConnection->setSearchPreferences(FALSE, 100);
+		
+		$searchResponse = $netsuiteClientConnection->search($contactRecordSearch);
+		
+		// var_dump($searchResponse);
+		
+		if($searchResponse->totalRecords > 0) {
+			if($searchResponse->totalRecords == 1) {
+				// then we have found a match, modify / update existing information
+				$currentRecord = $searchResponse->recordList[0];
+				return $currentRecord->getField('internalId');
+			} else {
+				return $searchResponse->recordList;
+			}
+		} else {
+			return FALSE;
+		}
+	}
+	
+	public static function createContact($data) {
+		
+	}
+}
+?>
